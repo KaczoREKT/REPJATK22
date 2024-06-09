938bbd163e8a364d2b418de4a12684d4bc998d73
s28876 <s28876@pjwstk.edu.pl>
2024-06-10 00:19:21 +0200

    "Dodano MaxMethod"

--- ConsoleApp1/ConsoleApp1/MaxMethod.cs
+++ ConsoleApp1/ConsoleApp1/MaxMethod.cs
@@ -1,6 +1,23 @@
 namespace DefaultNamespace;
 
-public class MaxMethod
-{
+public class AverageMethod
+{ 
+    public static double calculateMax(int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            double max = 0;
+            foreach (int number in numbers)
+            {
+                if number > max
+                {
+                    max = number
+                }
+            }
+
+            return max;
+        }
     
-}
